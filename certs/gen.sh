3b8aa494df0a14687416ca221cc8fdbbe02e642d
Tali Auster <me@tali.network>
2025-11-19 11:14:01 -0800

    fix(certs): use SAN instead of CN

--- certs/gen.sh
+++ certs/gen.sh
@@ -7,4 +7,7 @@ function die() {
 
 [ -z "$1" ] && die "FQDN required as first argument."
 
-openssl req -x509 -newkey rsa -noenc -keyout "$1".key -days 45 -out "$1".cert
+openssl req \
+	-x509 -newkey rsa -noenc -keyout "$1".key -days 45 -out "$1".cert \
+	-subj "/C=US/ST=Oregon/O=The Reed Institute" \
+	-addext "subjectAltName=DNS:$1"
